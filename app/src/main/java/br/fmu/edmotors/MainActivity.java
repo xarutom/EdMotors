09b952249ffff9be91fa58838be7555308cf526f
PEDRO HENRIQUE ALVES DOS SANTOS <2637346@fmu.interno>
2022-10-31 21:28:02 -0300

    TelaValoresUpdate

--- app/src/main/java/br/fmu/edmotors/MainActivity.java
+++ app/src/main/java/br/fmu/edmotors/MainActivity.java
@@ -23,4 +23,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
+
+    public void abrirValores(View view){
+        Intent intent = new Intent(this, ValoresActivity.class);
+        startActivity(intent);
+    }
 }
