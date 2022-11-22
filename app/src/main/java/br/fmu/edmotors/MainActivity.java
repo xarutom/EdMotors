cf243498844bec3cd9936518f0c1db62b0b9656f
Gustavo Carvalho <gutyribeiro2015@gmail.com>
2022-11-21 21:02:23 -0300

    Validation

--- app/src/main/java/br/fmu/edmotors/MainActivity.java
+++ app/src/main/java/br/fmu/edmotors/MainActivity.java
@@ -5,13 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText editTextEntrar;
+    private EditText editTextPass;
+    private TextView textViewIncorreto;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextEntrar = findViewById(R.id.editTextEntrar);
+        editTextPass = findViewById(R.id.editTextPass);
+        textViewIncorreto = findViewById(R.id.textViewIncorreto);
+    }
+
+    @Override
+    public void setFinishOnTouchOutside(boolean finish) {
+        super.setFinishOnTouchOutside(finish);
     }
 
     public void abrirCadastro(View view){
@@ -20,8 +35,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void abrirHome(View view){
+        System.out.println("Voce clicou em entrar");
         Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
+
+        String Login = editTextEntrar.getText().toString();
+        Integer Senha = Integer.parseInt(editTextPass.getText().toString());
+        Login.toLowerCase();
+        if (Login.equals("Gustavo") && Senha == 123){
+            System.out.println("Autorizado");;
+            startActivity(intent);
+        }
+
+        else{
+            textViewIncorreto.setVisibility(View.VISIBLE);
+            System.out.println("Acesso Negado -"+Login+"-"+Senha);
+        }
     }
 
     public void abrirValores(View view){
